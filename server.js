a1b5cd90eb047897182e3c7ce1fc362a1695ddd5
mubasharbinafzal <31704893+mubasharbinafzal@users.noreply.github.com>
2019-09-11 18:09:33 +0500

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static(__dirname + '/public'));
 app.use('/api',appRoutes);
 
 
-mongoose.connect('mongodb://<mubasharafzal>:<chaudhary391>@ds229312.mlab.com:29312/tutorial',{ useNewUrlParser: true },function(err){
+mongoose.connect('mongodb://mubasharafzal:chaudhary391@ds229312.mlab.com:29312/tutorial',{ useNewUrlParser: true },function(err){
     if(err){
         console.log("Not connected to database"+ err);
     }else{
