ffcfba526f613d859f57ff3caa0f45107b6e685b
mubasharbinafzal <31704893+mubasharbinafzal@users.noreply.github.com>
2019-09-12 15:45:18 +0500

    Update api.js

--- app/routes/api.js
+++ app/routes/api.js
@@ -10,10 +10,8 @@ module.exports = function (router) {
     var options = {
       auth: {
       
-          //  api_user: 'mubashar1',
-           // api_key: 'Chaudhary391'
-            api_user: 'meanstackdevelpoer',
-            api_key: 'developerno1'
+            api_user: 'mubashar1',
+            api_key: 'Chaudhary391'
           //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
       }
     }
